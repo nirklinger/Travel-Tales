8bb195fbfee93116921470d19bdf7b7eafb48c63
Or Sam <orfacemailj@gmail.com>
2023-08-15 22:11:17 +0300

    fixing user env photo url

--- server/dal/users.ts
+++ server/dal/users.ts
@@ -11,7 +11,7 @@ import {
   Trips,
   Users,
 } from '../../types/db-schema-definitions';
-import {logger} from '../../utils/server-logger'
+import { logger } from '../../utils/server-logger';
 import { UserChangeRequest } from '../../types/awsTypes';
 import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider';
 import { ExternalUser } from '../../types/types';
@@ -22,45 +22,47 @@ export const PROFILE_PHOTO_FILE_NAME = 'profilePhoto.jpg';
 const PUBLIC_FOLDER = 'public';
 const BUCKET_NAME = process.env.BUCKET_NAME;
 const S3_REGION = process.env.AWS_REGION;
-
+const S3_URL = process.env.AWS_S3_URL;
 const isDevEnvironment = process.env.NODE_ENV === 'development';
 const client = new S3Client({
   region: S3_REGION,
 });
 
 export const getUserById = async (userId: string) => {
-    const connection = getConnection();
-    const users = await connection
+  const connection = getConnection();
+  const users = await connection
     .select<Users[]>(`${Table.Users}.*`)
     .from(Table.Users)
     .where('user_id', userId);
 
-    return users;
-}
+  if (!isDevEnvironment) {
+    return users.map(user => ({ ...user, avatar_photo: S3_URL + user.avatar_photo }));
+  }
+  return users;
+};
 
 export const getUserByExternalId = async (externalId: string) => {
   const isDevEnvironment = process.env.NODE_ENV === 'development';
   const connection = getConnection();
   const users = await connection
-  .select<Users[]>(`${Table.Users}.*`)
-  .from(Table.Users)
-  .where('external_id', externalId);
+    .select<Users[]>(`${Table.Users}.*`)
+    .from(Table.Users)
+    .where('external_id', externalId);
 
   if (!isDevEnvironment) {
     const envFitUser = users.map(userObject => {
-      return {...userObject, avatar_photo: `${process.env.AWS_S3_URL}${userObject.avatar_photo}`}
+      return { ...userObject, avatar_photo: `${process.env.AWS_S3_URL}${userObject.avatar_photo}` };
     });
     return envFitUser;
-
   }
   return users;
-}
+};
 
 export const insertUserToDb = async (newUser: Users | ExternalUser) => {
   console.log(`inserting user - ${JSON.stringify(newUser)}`);
-    const connection = getConnection();
-    await connection.insert(newUser).into(Table.Users).onConflict().ignore();
-}
+  const connection = getConnection();
+  await connection.insert(newUser).into(Table.Users).onConflict().ignore();
+};
 
 export const UpdateUserProfile = async (userId: number, updateData: Partial<Users>) => {
   const connection = getConnection();
@@ -68,23 +70,25 @@ export const UpdateUserProfile = async (userId: number, updateData: Partial<User
     .where('user_id', userId)
     .update(updateData, Object.keys(updateData));
 
-    return changes[0];
-}
+  return changes[0];
+};
 
 export const UpdateUserAttributesCommand = async (updateData: UserChangeRequest) => {
   const { userName, attribute } = updateData;
   const provider = new CognitoIdentityProvider({
-    region: process.env.AWS_REGION
+    region: process.env.AWS_REGION,
   });
   await provider.adminUpdateUserAttributes({
-    UserAttributes: [{
+    UserAttributes: [
+      {
         Name: attribute.key,
-        Value: attribute.value
-    }],
+        Value: attribute.value,
+      },
+    ],
     Username: userName,
     UserPoolId: process.env.AWS_USER_POOL_ID,
   });
-}
+};
 
 export const uploadUserProfilePhoto = async (userId: number, profilePhoto: formidable.File) => {
   const base64 = fs.readFileSync(profilePhoto.filepath, 'utf8');
